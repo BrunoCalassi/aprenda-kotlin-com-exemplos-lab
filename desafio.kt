af4140ee6118cf811171fea2918b916c8c38fdf7
BrunoCalassi <70973403+BrunoCalassi@users.noreply.github.com>
2024-01-30 17:37:36 -0300

    first commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(var nome: String, var idade: Int) {
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var descricao: String = "", val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO) {
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("${usuario.nome} matriculado na formação $nome de nível $nivel.")
+        } else {
+            println("${usuario.nome} já está matriculado na formação $nome de nível $nivel.")
+        }
     }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuario1 = Usuario("João", 25)
+    val usuario2 = Usuario("Maria", 30)
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", "Curso introdutório de Kotlin para iniciantes.", 90, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", "Aprenda os conceitos fundamentais de POO.", 120, Nivel.INTERMEDIARIO)
+
+    val formacao = Formacao("Desenvolvimento Kotlin", listOf(conteudo1, conteudo2), Nivel.INTERMEDIARIO)
+    val formacao2 = Formacao("Desenvolvimento Kotlin", listOf(conteudo1, conteudo2), Nivel.BASICO)
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao2.matricular(usuario1)
+    println("Inscritos na formação ${formacao.nome} (${formacao.nivel}):")
+    for (usuario in formacao.inscritos) {
+        println("- ${usuario.nome} com ${usuario.idade} anos de idade")
+    }
+    println("Inscritos na formação ${formacao2.nome} (${formacao2.nivel}):")
+    for (usuario in formacao2.inscritos) {
+        println("- ${usuario.nome} com ${usuario.idade} anos de idade")
+    }
 }
